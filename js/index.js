76ecf9cbc2b9d49ee32ed7c7c5e07ece0e79832c
Aarobh-Kumar <165626806+aarobhs087@users.noreply.github.com>
2024-07-30 19:59:46 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@
 
   var card = $('card'),
       openB = $('open'),
-      closeB = $('close'), 
+      closeB = $('close'),
       timer = null;
-
+  console.log('wat', card);
   openB.addEventListener('click', function () {
     card.setAttribute('class', 'open-half');
     if (timer) clearTimeout(timer);
@@ -19,11 +19,11 @@
 
   closeB.addEventListener('click', function () {
     card.setAttribute('class', 'close-half');
-    if (timer) clearTimeout(timer);
+    if (timer) clearTimerout(timer);
     timer = setTimeout(function () {
       card.setAttribute('class', '');
       timer = null;
     }, 1000);
   });
 
-})();
+}());
